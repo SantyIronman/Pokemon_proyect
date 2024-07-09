4922dbfbc28e07af22eaf1924958b837e253da14
Santiago <santiago.simancas30@gmail.com>
2024-07-09 00:05:28 -0400

    es una prueba para cargar la app para que se vea a publico

---
+++ vamos.py
@@ -0,0 +1,26 @@
+import streamlit as st 
+import pandas as pd
+import plotly_express as px
+
+st.set_page_config (page_title= "Santyapp", layout= "wide")
+
+
+with st.container():
+    st.header("hellooooooo")
+    st.title ("Mi gran app")
+    st.write("GUAPO, PODEROSO, ASOMBROSO")
+    st.empty()
+    
+with st.container ():
+    st.write ("---")
+    st.header ("CR7")
+    st.write("Cristiano Ronaldo")
+    st.image (r'C:\Users\Gamer Edition i5 Xtr\Desktop\Santy\Computación - 2\_102469653_gettyimages-962792890.jpg.webp', caption= 'Cristiano Ronaldo')
+
+with st.container():
+    st.write("---")
+    st.header ("Mi Margot") 
+    st.write ("Margot Robbie mi gran crush Harley Quinn")
+    st.image (r'C:\Users\Gamer Edition i5 Xtr\Desktop\Santy\Computación - 2\f.elconfidencial.com_original_619_1d5_d12_6191d5d12df3c69a30c1d39a753ca46f.jpg', caption= 'Harley Quinn')
+
+    
