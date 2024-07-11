b8c332718e7d17651e5db464c9f2c7a65efa6685
Santiago <santiago.simancas30@gmail.com>
2024-07-11 14:33:19 -0400

    feat: se anexa la primera pregunta de sql
    @naliethsilva,
    @willyjcm

---
+++ pregunta_1.sql
@@ -0,0 +1,15 @@
+--¿Cuáles son los promedios de los pokemones según sus puntos bases en cada una de sus generaciones?
+CREATE VIEW Pregunta_1 AS
+SELECT
+	generations.identifier AS "Generaciones",
+	stats.identifier AS "Puntos_de_Base",
+	count (pokemon_species.generation_id) AS "Total_de_Pokemon",
+	min(pokemon_stats.base_stat) AS "Valor_Minimo",
+	max(pokemon_stats.base_stat) AS "Valor_Maximo",
+	round (avg(pokemon_stats.base_stat), 2) AS "Promedio" 
+FROM pokemon_stats
+LEFT JOIN pokemon_species ON pokemon_stats.pokemon_id = pokemon_species.id
+LEFT JOIN generations ON pokemon_species.generation_id = generations.id
+LEFT JOIN stats ON pokemon_stats.stat_id = stats.id
+GROUP BY pokemon_stats.stat_id, "Generaciones"
+HAVING "Generaciones" IS NOT NULL;
