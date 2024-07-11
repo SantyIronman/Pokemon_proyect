ebbcc7a9a12f3c93d8cb23f00216b96fa3567b31
Santiago <santiago.simancas30@gmail.com>
2024-07-11 15:09:05 -0400

    feat: se anexa la correccion de la pregunta 3
    @naliethsilva
    @willyjcm
    @Andresaamo

---
+++ pregunta_4.sql
@@ -0,0 +1,17 @@
+--Promedio de las bases estadísticas según el tipo de Pokémon que no sean defectuosos
+CREATE VIEW Pregunta_4 AS 
+SELECT
+	types.identifier,
+	stats.identifier,
+	count(pokemon_types.type_id) AS "Total_de_Pokemon",
+	min(pokemon_stats.base_stat) AS "Minimo_de_Puntos",
+	max(pokemon_stats.base_stat) AS "Maximo_de_Puntos",
+	round(avg(pokemon_stats.base_stat), 2) AS "Promedio_de_Puntos"
+FROM pokemon_stats
+LEFT JOIN pokemon_types ON pokemon_stats.pokemon_id = pokemon_types.pokemon_id
+LEFT JOIN types ON pokemon_types.type_id = types.id
+LEFT JOIN stats ON pokemon_stats.stat_id = stats.id
+LEFT JOIN pokemon_species ON pokemon_species.id = pokemon_types.pokemon_id
+LEFT JOIN pokemon ON pokemon_stats.pokemon_id = pokemon.id
+WHERE pokemon.is_default = "0"
+GROUP BY types.identifier,stats.identifier;
