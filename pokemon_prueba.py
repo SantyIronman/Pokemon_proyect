3d87522aacb56302f965d48db852987ec3a67c43
Santiago <santiago.simancas30@gmail.com>
2024-07-10 20:14:01 -0400

    feat: se anexo el Marco Teorico del trabajo

--- pokemon_prueba.py
+++ pokemon_prueba.py
@@ -10,15 +10,53 @@ import os
 st.set_page_config (page_title= "Pokemon.app", layout= "wide")
 
 #Titulo para el proyecto, Planteamiento del Problema, Objetivos, Marco Teorico
-st.title("Análisis Estadístico de Pokémon desde la Generación I, II, III, IV")
+st.title("Análisis Estadístico de Pokémon desde la Generación I - IV")
 
 st.header ("Planteamiento del Problema")
 
-st.write ("Pokémon es una de las franquicias de videojuegos y serie de anime muy famosas. El universo Pokémon está separado por generaciones, cada generación cuenta con un grupo completamente nuevo de Pokémon de diferentes tipos, donde las personas más fanáticas de esta franquicia prefieren la primera generación de Pokémon aún así viendo Pokémon mejores que la primera generación, entonces es necesario conocer las estadísticas base de los Pokémon más resaltantes para conocer sobre este mundo. ")
+st.write ("Desde su introducción en 1996, los Pokémon han capturado la imaginación de millones de personas en todo el mundo. A lo largo de las primeras cuatro generaciones de juegos (Generación I a IV), se han introducido cientos de especies diferentes, cada una con características únicas que definen sus habilidades, estadísticas y tipología. Las personas más fanáticas de esta franquicia prefieren la primera generación de Pokémon aun así viendo Pokémon mejores que la primera generación, entonces es necesario realizar un análisis estadístico para proporcionar una comprensión más profunda de la estructura de los Pokémon a lo largo del tiempo.")
 
 st.header ("Objetivo")
 
 st.write ('<ul><li>Determinar el Pokémon con mayor estadística para cada base: salud, velocidad, defensa, ataque, defensa especial, ataque especial.</li><li>Identificar el Pokémon con mayor experiencia de pelea.</li><li>Determinar si el peso del Pokémon lleva a que el Pokémon tengan menor velocidad al momento de pelear.</li></ul', unsafe_allow_html= True)
+
+st.header ("Marco Teórico")
+
+st.write (""""
+<style>
+p {
+  text-align: justify;
+}
+</style>
+<p>Pokémon = Una contracción del japonés Poketto Monsutā, que significa monstruo de bolsillo.</p>
+<p>Pokémon es un proyecto que inció con la idea de crear un videojuego agradable para el público de los años 1990 cuando comenzaron a usarse consolas de videojuegos como el famoso “game boy”. En palabras de su creador:</p>
+<p>"Diría que cuando comenzamos teníamos fe absoluta en lo que estábamos haciendo y queríamos llevar a los niños la alegría de descubrir criaturas especiales —los Pokémon—, de coleccionarlos, de combatir con ellos y de intercambiarlos: ese era nuestro sueño” – Junichi Masuda.</p>
+<p>Los pokémon son criaturas de diferentes tipos y con diferentes habilidades. Actualmente en el universo del videojuego existen más de 800 criaturas ya que con cada generación que pasa esta lista se extiende con aproximadamente +80 pokémon por generación.</p>
+<p>En el videojuego los pokémon viven en diferentes hábitats o entre los humanos que los crían y cuidan de ellos (llamados también entrenadores). Durante el transcurso del juego los pokémon adquieren experiencia y evolucionan para convertirse en pokémon más fuertes.</p>
+<p>Cada pokémon cuenta con habilidades especiales iguales o diferentes a otros pokémon, para la cuarta generación hay más de 120 diferentes habilidades.</p>
+<p>Existen 18 tipos y cada pokemon puede ser de uno o dos tipos (Ej: Solo tierra o tierra y eléctrico)</p>
+<ul>
+    <li>•	Normal.</li>
+    <li>•	Lucha. </li>
+    <li>•	Volador.</li>
+    <li>•	Veneno.</li>
+    <li>•	Tierra.</li>
+    <li>•	Piedra.</li>
+    <li>•	Bicho.</li>
+    <li>•	Fantasma.</li>
+    <li>•	Acero.</li>
+    <li>•	Fuego.</li>
+    <li>•	Agua.</li>
+    <li>•	Planta.</li>
+    <li>•	Eléctrico.</li>
+    <li>•	Psíquico.</li>
+    <li>•	Hielo.</li>
+    <li>•	Dragón.</li>
+    <li>•	Oscuro.</li>
+    <li>•	Hada.</li>
+</ul>
+<p>En el universo de Pokémon existen 6 Generaciones: 1° Generación de 1996 con 151 Pokémon; 2° Generación de 1999 con 100 Pokémon; 3° Generación de 2002 con 135 Pokémon; 4° Generación de 2006 con 108 Pokémon; 5° Generación de 2010 con 156 Pokémon; 6° Generación de 2013 con 72 Pokémon.</p>
+""", unsafe_allow_html=True)
 
 #Pregunta 1
 with st.container():
